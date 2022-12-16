555581961183c5c12c8df6d2f155aca2d4617429
pao11 <2166037774@qq.com>
2022-12-16 22:26:06 +0800

    Initial commit

---
+++ lib_FFmpegAndroid/src/main/java/com/github/pao11/libffmpeg/FFmpegLoadLibraryAsyncTask.java
@@ -0,0 +1,66 @@
+package com.github.hiteshsondhi88.libffmpeg;
+
+import android.content.Context;
+import android.os.AsyncTask;
+
+import java.io.File;
+
+@Deprecated
+class FFmpegLoadLibraryAsyncTask extends AsyncTask<Void, Void, Boolean> {
+
+    private final String cpuArchNameFromAssets;
+    private final FFmpegLoadBinaryResponseHandler ffmpegLoadBinaryResponseHandler;
+    private final Context context;
+
+    FFmpegLoadLibraryAsyncTask(Context context, String cpuArchNameFromAssets, FFmpegLoadBinaryResponseHandler ffmpegLoadBinaryResponseHandler) {
+        this.context = context;
+        this.cpuArchNameFromAssets = cpuArchNameFromAssets;
+        this.ffmpegLoadBinaryResponseHandler = ffmpegLoadBinaryResponseHandler;
+    }
+
+    @Override
+    protected Boolean doInBackground(Void... params) {
+        File ffmpegFile = new File(FileUtils.getFFmpeg(context));
+        //2022-11-23
+//        if (ffmpegFile.exists() && isDeviceFFmpegVersionOld() && !ffmpegFile.delete()) {
+//            return false;
+//        }
+//        if (!ffmpegFile.exists()) {
+//            boolean isFileCopied = FileUtils.copyBinaryFromAssetsToData(context,
+//                    cpuArchNameFromAssets + File.separator + FileUtils.ffmpegFileName,
+//                    FileUtils.ffmpegFileName);
+//
+//            // make file executable
+//            if (isFileCopied) {
+//                if(!ffmpegFile.canExecute()) {
+//                    Log.d("FFmpeg is not executable, trying to make it executable ...");
+//                    if (ffmpegFile.setExecutable(true)) {
+//                        return true;
+//                    }
+//                } else {
+//                    Log.d("FFmpeg is executable");
+//                    return true;
+//                }
+//            }
+//        }
+//        return ffmpegFile.exists() && ffmpegFile.canExecute();
+        return ffmpegFile.exists();
+    }
+
+    @Override
+    protected void onPostExecute(Boolean isSuccess) {
+        super.onPostExecute(isSuccess);
+        if (ffmpegLoadBinaryResponseHandler != null) {
+            if (isSuccess) {
+                ffmpegLoadBinaryResponseHandler.onSuccess();
+            } else {
+                ffmpegLoadBinaryResponseHandler.onFailure();
+            }
+            ffmpegLoadBinaryResponseHandler.onFinish();
+        }
+    }
+
+    private boolean isDeviceFFmpegVersionOld() {
+        return CpuArch.fromString(FileUtils.SHA1(FileUtils.getFFmpeg(context))).equals(CpuArch.NONE);
+    }
+}
